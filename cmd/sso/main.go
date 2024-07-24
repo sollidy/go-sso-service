debaa910aebac516beb47a3830c370933aac1e7d
Mark Pavlov <m7mark@yandex.ru>
2024-07-25 00:31:01 +0300

    add slog logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,9 +1,42 @@
-package sso
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso-service/internal/config"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-
-	// TODO: config
-	// TODO: logger
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Debug("starting application", slog.Any("cfg", cfg))
 	// TODO: init app
 	// TODO: start app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	tintHandler := tint.NewHandler(os.Stdout, &tint.Options{
+		Level:      slog.LevelDebug,
+		TimeFormat: time.TimeOnly,
+	})
+
+	switch env {
+	case envLocal:
+		log = slog.New(tintHandler)
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
