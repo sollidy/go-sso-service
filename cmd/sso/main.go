5f1a3c40ee87e639cb11dc61b6aaf1cecc4ecec1
Mark Pavlov <m7mark@yandex.ru>
2024-08-19 18:02:30 +0300

    refactor graceful shutdown

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -15,29 +15,29 @@ import (
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
 	cfg := config.MustLoad()
 	log := logger.SetupLogger(cfg.Env)
 	log.Debug("starting application", slog.Any("cfg", cfg))
-	ctx, cancel := context.WithCancel(context.Background())
 
 	application := app.New(log, cfg.GRPC.Port, cfg.TokenTTL)
 
 	go application.GRPCSrv.MustRun()
-
 	go application.Storage.MustConnect()
-
 	application.Sender.StartProcessingEvents(ctx, 5*time.Second)
 
-	// Graceful shutdown
+	shutdown(cancel, application, log) // graceful shutdown
+}
 
+func shutdown(cancel context.CancelFunc, app *app.App, log *slog.Logger) {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 
 	sign := <-stop
 
 	cancel()
-	application.GRPCSrv.Stop()
-	application.Storage.Close()
+	app.GRPCSrv.Stop()
+	app.Storage.Close()
 
 	log.Warn("STOPED application", slog.String("signal", sign.String()))
 }
